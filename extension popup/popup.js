52129938352abb3affa99b43fbffcf9854428da5
silver-cpu <81588234+silver-cpu@users.noreply.github.com>
2023-10-06 21:35:54 -0400

    Update popup.js

--- extension popup/popup.js
+++ extension popup/popup.js
@@ -1,17 +1,5 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
+/*
 const tabs = await chrome.tabs.query({
   url: [
     'https://developer.chrome.com/docs/webstore/*',
@@ -19,7 +7,6 @@ const tabs = await chrome.tabs.query({
   ]
 });
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
 
@@ -42,7 +29,8 @@ for (const tab of tabs) {
   elements.add(element);
 }
 document.querySelector('ul').append(...elements);
-
+*/
+// v what the button does v
 var button = document.getElementById('button');
 button.addEventListener('click', change);
 function change() {
